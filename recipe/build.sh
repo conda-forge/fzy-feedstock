850577787e019947fa8a0bd2d29b2184b20519cd
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-03 13:02:39 -0700

    skip check when cross-compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,7 @@
 set -o xtrace -o nounset -o pipefail -o errexit
 
 make -j${CPU_COUNT}
-make -j${CPU_COUNT} test
+if [[ ${CONDA_BUILD_CROSS_COMPILATION:-0} == 0 ]]; then
+    make -j${CPU_COUNT} test
+fi
 make -j${CPU_COUNT} PREFIX=${PREFIX} install
